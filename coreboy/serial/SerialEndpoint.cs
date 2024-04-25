d5b7e3295db445c52a4580abdc8f0d044c22847e
svedev0 <emanuel@kulich.me>
2024-04-25 10:39:16 +0200

    Revert "refactor coreboy.serial"
    This reverts commit 2874b36727510c70039f24a0fc7b9ae2c3edfe90.

--- coreboy/serial/SerialEndpoint.cs
+++ coreboy/serial/SerialEndpoint.cs
@@ -1,14 +1,16 @@
-namespace coreboy.serial;
-
-public interface ISerialEndpoint
+namespace coreboy.serial
 {
-	int Transfer(int outgoing);
-}
+    public interface SerialEndpoint
+    {
+        int transfer(int outgoing);
+    }
 
-public class NullSerialEndpoint : ISerialEndpoint
-{
-	public int Transfer(int outgoing)
-	{
-		return 0;
-	}
+
+    public class NullSerialEndpoint : SerialEndpoint
+    {
+        public int transfer(int outgoing)
+        {
+            return 0;
+        }
+    }
 }
--- coreboy/serial/SerialPort.cs
+++ coreboy/serial/SerialPort.cs
@@ -1,84 +1,92 @@
+using System;
+using System.Diagnostics;
+using System.IO;
 using coreboy.cpu;
 
-namespace coreboy.serial;
-
-public class SerialPort(
-	InterruptManager interruptManager,
-	ISerialEndpoint serialEndpoint,
-	SpeedMode speedMode) : IAddressSpace
+namespace coreboy.serial
 {
-	private readonly ISerialEndpoint _serialEndpoint = serialEndpoint;
-	private readonly InterruptManager _intManager = interruptManager;
-	private readonly SpeedMode _speedMode = speedMode;
-	private int _sb;
-	private int _sc;
-	private bool _transferInProgress;
-	private int _divider;
+    public class SerialPort : IAddressSpace
+    {
+        private readonly SerialEndpoint _serialEndpoint;
+        private readonly InterruptManager _interruptManager;
+        private readonly SpeedMode _speedMode;
+        private int _sb;
+        private int _sc;
+        private bool _transferInProgress;
+        private int _divider;
 
-	public void Tick()
-	{
-		if (!_transferInProgress)
-		{
-			return;
-		}
+        public SerialPort(InterruptManager interruptManager, SerialEndpoint serialEndpoint, SpeedMode speedMode)
+        {
+            _interruptManager = interruptManager;
+            _serialEndpoint = serialEndpoint;
+            _speedMode = speedMode;
+        }
 
-		if (_divider++ < Gameboy.TicksPerSec / 8192 / _speedMode.GetSpeedMode())
-		{
-			return;
-		}
+        public void Tick()
+        {
+            if (!_transferInProgress)
+            {
+                return;
+            }
+            
+            if (++_divider >= Gameboy.TicksPerSec / 8192 / _speedMode.GetSpeedMode())
+            {
+                _transferInProgress = false;
+                try
+                {
+                    _sb = _serialEndpoint.transfer(_sb);
+                }
+                catch (IOException e)
+                {
+                    Debug.WriteLine($"Can't transfer byte {e}");
+                    _sb = 0;
+                }
 
-		_transferInProgress = false;
+                _interruptManager.RequestInterrupt(InterruptManager.InterruptType.Serial);
+            }
+        }
 
-		try
-		{
-			_sb = _serialEndpoint.Transfer(_sb);
-		}
-		catch (IOException e)
-		{
-			Console.WriteLine($"Cannot transfer byte {e}");
-			_sb = 0;
-		}
+        public bool Accepts(int address)
+        {
+            return address == 0xff01 || address == 0xff02;
+        }
+        
+        public void SetByte(int address, int value)
+        {
+            if (address == 0xff01)
+            {
+                _sb = value;
+            }
+            else if (address == 0xff02)
+            {
+                _sc = value;
+                if ((_sc & (1 << 7)) != 0)
+                {
+                    StartTransfer();
+                }
+            }
+        }
 
-		_intManager.RequestInterrupt(InterruptManager.InterruptType.Serial);
-	}
+        public int GetByte(int address)
+        {
+            if (address == 0xff01)
+            {
+                return _sb;
+            }
+            else if (address == 0xff02)
+            {
+                return _sc | 0b01111110;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
 
-	public bool Accepts(int address)
-	{
-		return address == 0xff01 || address == 0xff02;
-	}
-
-	public void SetByte(int address, int value)
-	{
-		if (address == 0xff01)
-		{
-			_sb = value;
-			return;
-		}
-
-		if (address == 0xff02)
-		{
-			_sc = value;
-		}
-
-		if ((_sc & (1 << 7)) != 0)
-		{
-			StartTransfer();
-		}
-	}
-
-	public int GetByte(int address)
-	{
-		return address switch
-		{
-			0xff01 => _sb,
-			0xff02 => _sc | 0b01111110,
-			_ => throw new ArgumentException("Illegal address for serial port")
-		};
-	}
-
-	private void StartTransfer()
-	{
-		_transferInProgress = true;
-		_divider = 0;
-	}
+        private void StartTransfer()
+        {
+            _transferInProgress = true;
+            _divider = 0;
+        }
+    }
 }
