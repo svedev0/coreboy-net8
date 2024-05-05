5dda29c2ae556531a89ebaf8a55f5aebf02ad78b
svedev0 <emanuel@kulich.me>
2024-05-05 20:27:51 +0200

    refactor coreboy.memory.cart

--- coreboy/memory/cart/Cartridge.cs
+++ coreboy/memory/cart/Cartridge.cs
@@ -1,201 +1,185 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
 using System.Text;
 using coreboy.memory.cart.battery;
 using coreboy.memory.cart.type;
 
-namespace coreboy.memory.cart
+namespace coreboy.memory.cart;
+
+public class Cartridge : IAddressSpace
 {
-	public class Cartridge : IAddressSpace
+	public enum GameboyTypeFlag
 	{
-		[SuppressMessage("ReSharper", "InconsistentNaming")]
-		public enum GameboyTypeFlag
+		UNIVERSAL = 0x80,
+		GBC = 0xc0,
+		NON_GBC = 0
+	}
+
+	public bool Gbc { get; }
+	public string Title { get; }
+
+	private readonly IAddressSpace _addressSpace;
+
+	private int dmgBootstrap;
+
+	public Cartridge(GameboyOptions options)
+	{
+		var file = options.RomFile;
+		int[] rom = LoadFile(file);
+		var type = CartridgeTypeExtensions.GetById(rom[0x0147]);
+
+		Title = GetTitle(rom);
+
+		var gameboyType = GetFlag(rom[0x0143]);
+		int romBanks = GetRomBanks(rom[0x0148]);
+		int ramBanks = GetRamBanks(rom[0x0149]);
+
+		if (ramBanks == 0 && type.IsRam())
 		{
-			UNIVERSAL = 0x80,
-			GBC = 0xc0,
-			NON_GBC = 0
+			ramBanks = 1;
 		}
 
-		private readonly IAddressSpace _addressSpace;
-		private int _dmgBootstrap;
+		IBattery battery = new NullBattery();
 
-		public bool Gbc { get; }
-		public string Title { get; }
-
-		public Cartridge(GameboyOptions options)
+		if (type.IsBattery() && options.IsSupportBatterySaves())
 		{
-			var file = options.RomFile;
-			var rom = LoadFile(file);
-			var type = CartridgeTypeExtensions.GetById(rom[0x0147]);
-
-			Title = GetTitle(rom);
-			// LOG.debug("Cartridge {}, type: {}", title, type);
-
-			var gameboyType = GetFlag(rom[0x0143]);
-			var romBanks = GetRomBanks(rom[0x0148]);
-			var ramBanks = GetRamBanks(rom[0x0149]);
-
-			if (ramBanks == 0 && type.IsRam())
-			{
-				// LOG.warn("RAM bank is defined to 0. Overriding to 1.");
-				ramBanks = 1;
-			}
-			// LOG.debug("ROM banks: {}, RAM banks: {}", romBanks, ramBanks);
-
-			IBattery battery = new NullBattery();
-			if (type.IsBattery() && options.IsSupportBatterySaves())
-			{
-				//throw new NotImplementedException("Implement battery loading");
-				battery = new FileBattery(file?.Name ?? string.Empty);
-			}
-
-			if (type.IsMbc1())
-			{
-				_addressSpace = new Mbc1(rom, type, battery, romBanks, ramBanks);
-			}
-			else if (type.IsMbc2())
-			{
-				_addressSpace = new Mbc2(rom, type, battery, romBanks);
-			}
-			else if (type.IsMbc3())
-			{
-				_addressSpace = new Mbc3(rom, type, battery, romBanks, ramBanks);
-			}
-			else if (type.IsMbc5())
-			{
-				_addressSpace = new Mbc5(rom, type, battery, romBanks, ramBanks);
-			}
-			else
-			{
-				_addressSpace = new Rom(rom, type, romBanks, ramBanks);
-			}
-
-			_dmgBootstrap = options.UseBootstrap ? 0 : 1;
-
-			if (options.ForceGbc)
-			{
-				Gbc = true;
-				return;
-			}
-
-			switch (gameboyType)
-			{
-				case GameboyTypeFlag.NON_GBC:
-					Gbc = false;
-					break;
-				case GameboyTypeFlag.GBC:
-					Gbc = true;
-					break;
-				default:
-					// UNIVERSAL
-					Gbc = !options.ForceDmg;
-					break;
-			}
+			battery = new FileBattery(file?.Name ?? string.Empty);
 		}
 
-		private static string GetTitle(IReadOnlyList<int> rom)
+		if (type.IsMbc1())
 		{
-			var t = new StringBuilder();
-			for (var i = 0x0134; i < 0x0143; i++)
-			{
-				var c = (char)rom[i];
-				if (c == 0)
-				{
-					break;
-				}
+			_addressSpace = new Mbc1(rom, type, battery, romBanks, ramBanks);
+		}
+		else if (type.IsMbc2())
+		{
+			_addressSpace = new Mbc2(rom, type, battery, romBanks);
+		}
+		else if (type.IsMbc3())
+		{
+			_addressSpace = new Mbc3(rom, type, battery, romBanks, ramBanks);
+		}
+		else if (type.IsMbc5())
+		{
+			_addressSpace = new Mbc5(rom, type, battery, romBanks, ramBanks);
+		}
+		else
+		{
+			_addressSpace = new Rom(rom, type, romBanks, ramBanks);
+		}
 
-				t.Append(c);
+		dmgBootstrap = options.UseBootstrap ? 0 : 1;
+
+		if (options.ForceGbc)
+		{
+			Gbc = true;
+			return;
+		}
+
+		Gbc = gameboyType switch
+		{
+			GameboyTypeFlag.NON_GBC => false,
+			GameboyTypeFlag.GBC => true,
+			_ => !options.ForceDmg // UNIVERSAL
+		};
+	}
+
+	private static string GetTitle(IReadOnlyList<int> rom)
+	{
+		StringBuilder sb = new();
+
+		for (int i = 0x0134; i < 0x0143; i++)
+		{
+			char c = (char)rom[i];
+
+			if (c == 0)
+			{
+				break;
 			}
 
-			return t.ToString();
+			sb.Append(c);
 		}
 
-		public bool Accepts(int address) => _addressSpace.Accepts(address) || address == 0xff50;
+		return sb.ToString();
+	}
 
-		public void SetByte(int address, int value)
+	public bool Accepts(int address)
+	{
+		return _addressSpace.Accepts(address) || address == 0xff50;
+	}
+
+	public void SetByte(int address, int value)
+	{
+		if (address == 0xff50)
 		{
-			if (address == 0xff50)
-			{
-				_dmgBootstrap = 1;
-			}
-			else
-			{
-				_addressSpace.SetByte(address, value);
-			}
+			dmgBootstrap = 1;
 		}
-
-
-		public int GetByte(int address)
+		else
 		{
-			switch (_dmgBootstrap)
-			{
-				case 0 when !Gbc && (address >= 0x0000 && address < 0x0100):
-					return BootRom.GameboyClassic[address];
-				case 0 when Gbc && address >= 0x000 && address < 0x0100:
-					return BootRom.GameboyColor[address];
-				case 0 when Gbc && address >= 0x200 && address < 0x0900:
-					return BootRom.GameboyColor[address - 0x0100];
-			}
-
-			return address == 0xff50 ? 0xff : _addressSpace.GetByte(address);
+			_addressSpace.SetByte(address, value);
 		}
+	}
 
-		private static int[] LoadFile(FileSystemInfo? file)
+	public int GetByte(int address)
+	{
+		return dmgBootstrap switch
 		{
-			// string ext = file.Extension;
-			// TODO: If file is a zip, try extract gb, gbc or rom file to play
-			// Deleted original java impl
+			0 when !Gbc && address >= 0x0000 && address < 0x0100 =>
+				BootRom.GameboyClassic[address],
+			0 when Gbc && address >= 0x000 && address < 0x0100 =>
+				BootRom.GameboyColor[address],
+			0 when Gbc && address >= 0x200 && address < 0x0900 =>
+				BootRom.GameboyColor[address - 0x0100],
+			_ => address == 0xff50 ? 0xff : _addressSpace.GetByte(address),
+		};
+	}
 
-			if (file == null)
-			{
-				throw new Exception("Cartridge file is null");
-			}
-
-			return File.ReadAllBytes(file.FullName).Select(x => (int)x).ToArray();
-		}
-
-		private static int GetRomBanks(int id)
+	private static int[] LoadFile(FileSystemInfo? file)
+	{
+		if (file == null)
 		{
-			return id switch
-			{
-				0 => 2,
-				1 => 4,
-				2 => 8,
-				3 => 16,
-				4 => 32,
-				5 => 64,
-				6 => 128,
-				7 => 256,
-				0x52 => 72,
-				0x53 => 80,
-				0x54 => 96,
-				_ => throw new ArgumentException("Unsupported ROM size: " + Integer.ToHexString(id))
-			};
+			throw new Exception("Cartridge file is null");
 		}
 
-		private static int GetRamBanks(int id)
+		return File.ReadAllBytes(file.FullName).Select(x => (int)x).ToArray();
+	}
+
+	private static int GetRomBanks(int id)
+	{
+		return id switch
 		{
-			return id switch
-			{
-				0 => 0,
-				1 => 1,
-				2 => 1,
-				3 => 4,
-				4 => 16,
-				_ => throw new ArgumentException("Unsupported RAM size: " + Integer.ToHexString(id))
-			};
-		}
-		public static GameboyTypeFlag GetFlag(int value)
+			0 => 2,
+			1 => 4,
+			2 => 8,
+			3 => 16,
+			4 => 32,
+			5 => 64,
+			6 => 128,
+			7 => 256,
+			0x52 => 72,
+			0x53 => 80,
+			0x54 => 96,
+			_ => throw new ArgumentException("Unsupported ROM size")
+		};
+	}
+
+	private static int GetRamBanks(int id)
+	{
+		return id switch
 		{
-			return value switch
-			{
-				0x80 => GameboyTypeFlag.UNIVERSAL,
-				0xc0 => GameboyTypeFlag.GBC,
-				_ => GameboyTypeFlag.NON_GBC
-			};
-		}
+			0 => 0,
+			1 => 1,
+			2 => 1,
+			3 => 4,
+			4 => 16,
+			_ => throw new ArgumentException("Unsupported RAM size")
+		};
+	}
+
+	public static GameboyTypeFlag GetFlag(int value)
+	{
+		return value switch
+		{
+			0x80 => GameboyTypeFlag.UNIVERSAL,
+			0xc0 => GameboyTypeFlag.GBC,
+			_ => GameboyTypeFlag.NON_GBC
+		};
 	}
 }
--- coreboy/memory/cart/CartridgeTypeExtensions.cs
+++ coreboy/memory/cart/CartridgeTypeExtensions.cs
@@ -1,32 +1,72 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace coreboy.memory.cart
+namespace coreboy.memory.cart;
+
+public static class CartridgeTypeExtensions
 {
-    public static class CartridgeTypeExtensions
-    {
-        public static IEnumerable<CartridgeType> Values(this CartridgeType src)
-        {
-            return Enum.GetValues(typeof(CartridgeType)).Cast<CartridgeType>();
-        }
+	public static IEnumerable<CartridgeType> Values(this CartridgeType src)
+	{
+		return Enum.GetValues(typeof(CartridgeType)).Cast<CartridgeType>();
+	}
 
-        public static bool IsMbc1(this CartridgeType src) => src.NameContainsSegment("MBC1");
-        public static bool IsMbc2(this CartridgeType src) => src.NameContainsSegment("MBC2");
-        public static bool IsMbc3(this CartridgeType src) => src.NameContainsSegment("MBC3");
-        public static bool IsMbc5(this CartridgeType src) => src.NameContainsSegment("MBC5");
-        public static bool IsMmm01(this CartridgeType src) => src.NameContainsSegment("MMM01");
-        public static bool IsRam(this CartridgeType src) => src.NameContainsSegment("RAM");
-        public static bool IsSram(this CartridgeType src) => src.NameContainsSegment("SRAM");
-        public static bool IsTimer(this CartridgeType src) => src.NameContainsSegment("TIMER");
-        public static bool IsBattery(this CartridgeType src) => src.NameContainsSegment("BATTERY");
-        public static bool IsRumble(this CartridgeType src) => src.NameContainsSegment("RUMBLE");
-        private static bool NameContainsSegment(this CartridgeType src, string segment)
-        {
-            return new Regex("(^|_)" + Regex.Escape(segment) + "($|_)").IsMatch(src.ToString());
-        }
+	public static bool IsMbc1(this CartridgeType src)
+	{
+		return src.NameContainsSegment("MBC1");
+	}
 
-        public static CartridgeType GetById(int id) => (CartridgeType) id;
-    }
+	public static bool IsMbc2(this CartridgeType src)
+	{
+		return src.NameContainsSegment("MBC2");
+	}
+
+	public static bool IsMbc3(this CartridgeType src)
+	{
+		return src.NameContainsSegment("MBC3");
+	}
+
+	public static bool IsMbc5(this CartridgeType src)
+	{
+		return src.NameContainsSegment("MBC5");
+	}
+
+	public static bool IsMmm01(this CartridgeType src)
+	{
+		return src.NameContainsSegment("MMM01");
+	}
+
+	public static bool IsRam(this CartridgeType src)
+	{
+		return src.NameContainsSegment("RAM");
+	}
+
+	public static bool IsSram(this CartridgeType src)
+	{
+		return src.NameContainsSegment("SRAM");
+	}
+
+	public static bool IsTimer(this CartridgeType src)
+	{
+		return src.NameContainsSegment("TIMER");
+	}
+
+	public static bool IsBattery(this CartridgeType src)
+	{
+		return src.NameContainsSegment("BATTERY");
+	}
+
+	public static bool IsRumble(this CartridgeType src)
+	{
+		return src.NameContainsSegment("RUMBLE");
+	}
+
+	private static bool NameContainsSegment(this CartridgeType src, string segment)
+	{
+		return new Regex("(^|_)" +
+			Regex.Escape(segment) + "($|_)").IsMatch(src.ToString());
+	}
+
+	public static CartridgeType GetById(int id)
+	{
+		return (CartridgeType)id;
+	}
 }
