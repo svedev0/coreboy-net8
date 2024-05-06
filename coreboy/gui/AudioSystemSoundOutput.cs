1b85effe90fbc6f3ad87597a1499918fd48ef937
svedev0 <emanuel@kulich.me>
2024-05-06 22:05:38 +0200

    added AudioSystemSoundOutput

---
+++ coreboy/gui/AudioSystemSoundOutput.cs
@@ -0,0 +1,93 @@
+﻿#nullable disable
+
+using NAudio.Wave;
+using coreboy.sound;
+
+namespace coreboy.gui;
+
+public class AudioSystemSoundOutput : ISoundOutput
+{
+	private const int sampleRate = 22050;
+	private const int bufferSize = 1024;
+
+	private static readonly WaveFormat _format = new(sampleRate, 8, 2);
+
+	private BufferedWaveProvider bufferedWaveProvider;
+	private WaveOutEvent waveOut;
+
+	private byte[] buffer;
+	private int bufferIndex;
+
+	private int tick;
+	private int divider;
+
+	public void Start()
+	{
+		if (waveOut != null)
+		{
+			return;
+		}
+
+		try
+		{
+			waveOut = new WaveOutEvent();
+			bufferedWaveProvider = new BufferedWaveProvider(_format)
+			{
+				BufferLength = bufferSize * 4,
+				DiscardOnBufferOverflow = true
+			};
+			waveOut.Init(bufferedWaveProvider);
+		}
+		catch
+		{
+			throw new InvalidOperationException("Failed to start sound output");
+		}
+
+		waveOut.Play();
+		buffer = new byte[bufferSize];
+		divider = Gameboy.TicksPerSec / sampleRate;
+	}
+
+	public void Stop()
+	{
+		if (waveOut == null)
+		{
+			return;
+		}
+		
+		waveOut.Stop();
+		waveOut.Dispose();
+		waveOut = null;
+	}
+
+	public void Play(int left, int right)
+	{
+		tick++;
+
+		if (tick != 0)
+		{
+			tick %= divider;
+			return;
+		}
+
+		ValidateRange(left, 0, 255);
+		ValidateRange(right, 0, 255);
+
+		buffer[bufferIndex++] = (byte)left;
+		buffer[bufferIndex++] = (byte)right;
+
+		if (bufferIndex >= bufferSize / 2)
+		{
+			bufferedWaveProvider.AddSamples(buffer, 0, bufferIndex);
+			bufferIndex = 0;
+		}
+	}
+
+	private static void ValidateRange(int value, int min, int max)
+	{
+		if (value < min || value > max)
+		{
+			throw new ArgumentException("Value is out of range");
+		}
+	}
+}
